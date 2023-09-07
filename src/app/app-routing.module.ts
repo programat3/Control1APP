f4cb7a37a964a3c53a53d7ce9d6953c6332f212d
programat3 <li.dasilva@duocuc.cl>
2023-09-07 20:23:51 -0300

    recover -> recuperar contraseña

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
+  {
+    path: 'recover',
+    loadChildren: () => import('./pages/recover/recover.module').then( m => m.RecoverPageModule)
+  },
 ];
 
 @NgModule({
