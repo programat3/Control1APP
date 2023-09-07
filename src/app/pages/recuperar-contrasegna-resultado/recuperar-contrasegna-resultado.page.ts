51a712fdcf4e40fac33069d471f1e60a1c2654a2
programat3 <li.dasilva@duocuc.cl>
2023-09-07 20:35:18 -0300

    cambios?

---
+++ src/app/pages/recuperar-contrasegna-resultado/recuperar-contrasegna-resultado.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recuperar-contrasegna-resultado',
+  templateUrl: './recuperar-contrasegna-resultado.page.html',
+  styleUrls: ['./recuperar-contrasegna-resultado.page.scss'],
+})
+export class RecuperarContrasegnaResultadoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
