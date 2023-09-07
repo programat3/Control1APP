22cc77515ac6194e528aca75b20c942e3a9ded63
programat3 <li.dasilva@duocuc.cl>
2023-09-07 17:25:58 -0300

    cambios para QR, aún no funcionales

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,8 +1,9 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { AlertController, LoadingController } from '@ionic/angular';
 import { AnimationController} from '@ionic/angular';
 import { Usuario } from 'src/app/model/Usuario';
+import jsQR, { QRCode } from 'jsqr';
 
 @Component({
   selector: 'app-home',
@@ -15,11 +16,15 @@ export class HomePage implements OnInit, AfterViewInit{
   @ViewChild('titulo', { read: ElementRef, static: true}) titulo!: ElementRef;
   @ViewChild('nombre', { read: ElementRef, static: true}) nombre!: ElementRef;
   @ViewChild('fileinput', {static: false}) fileinput!: ElementRef;
+  @ViewChild('video',{static: false}) video !: ElementRef;
+  @ViewChild('canvas',{static: false}) canvas !: ElementRef;
 
   public usuario: Usuario;
+  public escaneando = false;
+  public datosQR = '';
+  public loading: HTMLIonLoadingElement = new HTMLIonLoadingElement();
   
-  
-  constructor(
+  constructor(private loadingController : LoadingController,
     private route: ActivatedRoute,
       private router: Router,
       private animationController: AnimationController)
@@ -41,8 +46,100 @@ export class HomePage implements OnInit, AfterViewInit{
       animation.play();
   }
 
+  public obtenerDatosQR(source?: CanvasImageSource): boolean{
+    let w = 0;
+    let h = 0;
+    if(!source){
+      this.canvas.nativeElement.width = this.video.nativeElement.videoWitdth;
+      this.canvas.nativeElement.width = this.video.nativeElement.videoHeight;
+    }
+
+    w = this.canvas.nativeElement.width;
+    h = this.canvas.nativeElement.height;
+
+    const context: CanvasRenderingContext2D = this.canvas.nativeElement.getContext('2d');
+    context.drawImage(source? source : this.video.nativeElement, 0 , 0 , w, h);
+
+    const img: ImageData = context.getImageData(0,0,w,h);
+
+    const qrCode = jsQR(img.data, img.width, img.height, { inversionAttempts: 'dontInvert'});
+
+    if(qrCode){
+      this.escaneando = false;
+      this.datosQR = qrCode.data;
+    }
+
+    return(this.datosQR !== '');
+
+  }
+
+  public verificarArchivoConQR(event : Event){
+    try {
+      const element = event.currentTarget as HTMLInputElement;
+      let fileList: FileList | null = element.files;
+      if(fileList){
+        const file = fileList.item(0)
+        const img = new Image();
+        img.onload = () => {
+        this.obtenerDatosQR(img);
+      }
+      if(file){
+        img.src = URL.createObjectURL(file);
+      }
+      }
+      
+    } catch{}
+  }
   public cargarImagenDesdeArchivo() :void{
+    //this.limpiarDatos();
     this.fileinput.nativeElement.click();
+  }
+
+  public async comenzarEscaneoQR(){
+    //this.limpiarDatos();
+    const mediaProvider : MediaProvider = await navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: 'enviroment'
+      }
+    });
+    this.video.nativeElement.srcObject = mediaProvider;
+    this.video.nativeElement.setAttribute('playsinline', 'true');
+    this.loading = await this.loadingController.create({});
+    await this.loading.present();
+    this.video.nativeElement.play();
+
+    requestAnimationFrame(this.verificarVideo.bind(this));
+  }
+  public detenerEscaneoQR() : void{
+    this.escaneando = false;
+  }  
+
+  async verificarVideo(){
+    if(this.video.nativeElement.readyState ===  this.video.nativeElement.HAVE_ENOUGH_DATA){
+      if (this.loading){
+        await this.loading.dismiss();
+        this.loading = new HTMLIonLoadingElement;
+        this.escaneando = true;
+      }
+      if(this.obtenerDatosQR()){
+        console.log('datos obtenidos!');
+      }else{
+        if(this.escaneando){
+          console.log('escaneando...')
+          requestAnimationFrame(this.verificarVideo.bind(this));
+        }
+      }
+    } else{
+      console.log('video aún no tiene datos');
+      requestAnimationFrame(this.verificarVideo.bind(this));
+    }
+  }
+
+  public limpiarDatos(){
+    this.escaneando = false;
+    this.datosQR = '';
+    this.loading = new HTMLIonLoadingElement();
+    (document.getElementById('input-file') as HTMLInputElement).value = '';
   }
 
 }
