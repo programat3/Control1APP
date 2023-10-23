7ecc5d6e5d870c20324f15cc2e543e4318724d4b
programat3 <li.dasilva@duocuc.cl>
2023-10-23 18:58:38 -0300

    Segundo error del main resuelto

--- src/app/services/api-client.service.ts
+++ src/app/services/api-client.service.ts
@@ -1,79 +1,57 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { Usuario } from '../model/Usuario';
-import { Router } from '@angular/router';
-import { DataBaseService } from './data-base.service';
-import { Storage } from '@ionic/storage-angular';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Publicacion } from '../model/publicacion';
 import { showToast } from '../tools/message-routines';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class ApiClientService {
 
-  keyUsuario = 'USUARIO_AUTENTICADO';
-  usuarioAutenticado = new BehaviorSubject<Usuario | null>(null);
-  primerInicioSesion = new BehaviorSubject<boolean>(false);
-
-  constructor(private router: Router, private bd: DataBaseService, private storage: Storage) { 
-    this.inicializarAutenticacion();
-  }
-
-  async inicializarAutenticacion() {
-    await this.storage.create();
-  }
-
-  async isAuthenticated(): Promise<boolean> {
-    return await this.leerUsuarioAutenticado().then(usuario => {
-      return usuario !== null;
-    });
-  }
-
-  async leerUsuarioAutenticado(): Promise<Usuario | undefined> {
-    const usuario = await this.storage.get(this.keyUsuario) as Usuario;
-    this.usuarioAutenticado.next(usuario);
-    return usuario;
-  }
-
-  guardarUsuarioAutenticado(usuario: Usuario) {
-    this.storage.set(this.keyUsuario, usuario);
-    this.usuarioAutenticado.next(usuario);
-  }
-
-  eliminarUsuarioAutenticado(usuario: Usuario) {
-    this.storage.remove(this.keyUsuario);
-  }
-
-  async login(correo: string, password: string) {
-    await this.storage.get(this.keyUsuario).then( async (usuarioAutenticado) => {
-      if (usuarioAutenticado) {
-        this.usuarioAutenticado.next(usuarioAutenticado);
-        this.primerInicioSesion.next(false);
-        this.router.navigate(['inicio']);
-      } else {
-        await this.bd.validarUsuario(correo, password).then(async (usuario: Usuario | undefined) => {
-          if (usuario) {
-            showToast(`¡Bienvenido ${usuario.nombre}!`);
-            this.guardarUsuarioAutenticado(usuario);
-            this.primerInicioSesion.next(true);
-            this.router.navigate(['inicio']);
-          } else {
-            showToast(`El correo o la password son incorrectos`);
-            this.router.navigate(['ingreso']);
-          }
-        })
-      }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'content-type': 'application/json',
+      'access-control-allow-origin': '*'
     })
+  };
+
+  listaPublicaciones: BehaviorSubject<Publicacion[]> =  new BehaviorSubject<Publicacion[]>([]);
+  apiUrl = 'http://localhost:3000';
+  //apiUrl = 'http://192.168.100.34:3000';
+
+  constructor(private http: HttpClient) { }
+
+  async cargarPublicaciones() {
+    this.leerPublicaciones().subscribe({
+      next: (publicaciones) => {
+        this.listaPublicaciones.next(publicaciones as Publicacion[]);
+      },
+      error: (error: any) => {
+        showToast('El servicio API Rest de Publicaciones no está disponible');
+        this.listaPublicaciones.next([]);
+      }
+    });
   }
 
-  async logout() {
-    this.leerUsuarioAutenticado().then((usuario) => {
-      if (usuario) {
-        showToast(`¡Hasta pronto ${usuario.nombre}!`);
-        this.eliminarUsuarioAutenticado(usuario);
-      }
-      this.router.navigate(['ingreso']);
-    });
+  crearPublicacion(publicacion: any): Observable<any> {
+    return this.http.post(this.apiUrl + '/publicaciones/', publicacion, this.httpOptions);
+  }
+
+  leerPublicaciones(): Observable<any> {
+    return this.http.get(this.apiUrl + '/publicaciones/');
+  }
+
+  leerPublicacion(idPublicacion: number): Observable<any> {
+    return this.http.get(this.apiUrl + '/publicaciones/' + idPublicacion);
+  }
+
+  actualizarPublicacion(publicacion: any): Observable<any> {
+    return this.http.put(this.apiUrl + '/publicaciones/' + publicacion.id, publicacion, this.httpOptions);
+  }
+
+  eliminarPublicacion(publicacionId: number): Observable<any> {
+    return this.http.delete(this.apiUrl + '/publicaciones/' + publicacionId, this.httpOptions);
   }
 
 }
