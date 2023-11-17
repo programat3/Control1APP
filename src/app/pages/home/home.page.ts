bae3a6f4cd359788892a7bfef99deed875f44e44
ItsDani22 <dani.rodriguezr@duocuc.cl>
2023-11-17 09:46:19 -0300

    Animaciones hechas y te amo c:
    Eso, ¿dije que te amo?

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Animation, AnimationController } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ForoComponent } from 'src/app/components/foro/foro.component';
@@ -19,13 +20,25 @@ import { AuthService } from 'src/app/services/auth.service';
 
 export class HomePage implements OnInit, AfterViewInit {
   selectTabs = 'qr';
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService, private animationController: AnimationController) {
   }
+  @ViewChild('titulo', { read: ElementRef }) itemTitulo!: ElementRef;
 
   ngOnInit(): void {
   }
 
   public ngAfterViewInit(): void {
+    if (this.itemTitulo) {
+      const animation = this.animationController
+        .create()
+        .addElement(this.itemTitulo.nativeElement)
+        .iterations(Infinity)
+        .duration(6500)
+        .fromTo('transform', 'translate(-300px)', "translate(400px)")
+        .fromTo('opacity', 1, 0.2);
+
+      animation.play();
+    }
 
   }
 
