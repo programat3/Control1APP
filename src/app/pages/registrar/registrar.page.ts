8468016e888e309368b4e56e706ca8fb4a813d4d
ItsDani22 <dani.rodriguezr@duocuc.cl>
2023-11-25 21:49:17 -0300

    Cambios Pequis :C
    Me da miedo el Registrar

--- src/app/pages/registrar/registrar.page.ts
+++ src/app/pages/registrar/registrar.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { DataBaseService } from 'src/app/services/data-base.service';
+import { Usuario } from 'src/app/model/Usuario';
 
 @Component({
   selector: 'app-registrar',
@@ -8,9 +11,19 @@ import { Router } from '@angular/router';
 })
 export class RegistrarPage implements OnInit {
 
-  constructor(private router: Router) { }
+  usuario = new Usuario();
+  repeticionPassword = '';
+
+  constructor(private auth: AuthService, private bd: DataBaseService, private router: Router) { }
 
   ngOnInit() {
+    this.auth.usuarioAutenticado.subscribe((usuario) => {
+      this.usuario = usuario ? usuario : new Usuario();
+      this.repeticionPassword = usuario ? usuario.password : '';
+    });
+  }
+  async crearPerfil() {
+
   }
 
   volverLogin() {
