4e72db50a90db659e26573f394325b0d1484f10d
programat3 <li.dasilva@duocuc.cl>
2023-11-27 11:45:40 -0300

    admin component casi

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { Usuario } from 'src/app/model/Usuario';
+import { AuthService } from 'src/app/services/auth.service';
+import { DataBaseService } from 'src/app/services/data-base.service';
 
 @Component({
   selector: 'app-admin',
@@ -11,8 +15,10 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  private usuarios: any = [];
+  constructor(private auth: AuthService, private bd: DataBaseService, private router: Router) { 
+    this.usuarios = this.bd.leerUsuarios;
+  }
 
   ngOnInit() { }
 
