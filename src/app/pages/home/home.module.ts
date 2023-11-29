40efd2b4eb5fa04f2f35aea0d61e6a846955b2c4
ItsDani22 <dani.rodriguezr@duocuc.cl>
2023-11-28 22:27:04 -0300

    Mini limpieza código

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,14 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
-import { QrComponent } from 'src/app/components/qr/qr.component';
-import { MiclaseComponent } from 'src/app/components/miclase/miclase.component';
-import { ForoComponent } from 'src/app/components/foro/foro.component';
-import { MisDatosComponent } from 'src/app/components/mis-datos/mis-datos.component';
-import { AdminComponent } from 'src/app/components/admin/admin.component';
 
 
 @NgModule({
